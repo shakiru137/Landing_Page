3b8ab24536deedaf00be7adacb2df0c4cad646ab
oluwasegun1379 <yusufshakiruoluwasegun137@gmail.com>
2024-07-15 12:38:41 +0100

    Updated

---
+++ script.js
@@ -0,0 +1,21 @@
+// Add any JavaScript interactivity here
+
+// Example: Smooth scroll for navigation links
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll("header nav ul li a");
+
+  for (const link of links) {
+    link.addEventListener("click", smoothScroll);
+  }
+
+  function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    window.scrollTo({
+      top: targetSection.offsetTop,
+      behavior: "smooth",
+    });
+  }
+});
